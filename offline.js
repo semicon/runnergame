2c540a1ba8c8ab7c84b2ae3dfdafd739650b2f7b
guruchian <30399464+semicon@users.noreply.github.com>
2021-02-24 09:09:53 +0700

    runnergame

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1614132438,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ground.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/downbtn-sheet0.png",
+		"images/upbtn-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/pac-sheet0.png",
+		"images/pac-sheet1.png",
+		"images/textscore.png",
+		"images/hp-sheet0.png",
+		"images/hp-sheet1.png",
+		"images/restart-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
